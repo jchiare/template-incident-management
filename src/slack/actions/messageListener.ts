265a002fce7024d537e8738db71e1ec25e70301c
Jay Chiarella <jay.gch93@gmail.com>
2021-03-21 21:56:11 +0100

    [master] better naming
    on branch [master]

---
+++ src/slack/actions/messageListener.ts
@@ -0,0 +1,13 @@
+import { App } from "@slack/bolt"
+
+interface MessageInterface {
+    message?:any;
+    say?:any;
+}
+
+export default function messageListener(app:App):void{
+    app.message('hey', async ({ message, say }:MessageInterface) => {
+        await say(`Hello, <@${message.user}>`);
+    });
+}
+
